ee36abebaaff45d5883962992b66994106352f2c
byron <byron@byrongibby.online>
2023-06-06 10:07:59 +0200

    Changed tab order
    Also changed log-diff numbers to %ch

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 # %%
 import dash_bootstrap_components as dbc
+import numpy as np
 
 # %% 
 import pandas as pd
@@ -78,10 +79,43 @@ app.layout = dbc.Container([
         className='custom-tabs-container',
         # className="dash-bootstrap",
         children=[
-        dcc.Tab(label='Nowcast Current Quarter', children=[
+        dcc.Tab(label='Nowcasts', children=[
+                html.Br(),
+                html.H5("All Nowcasts and News Releases"),
+                html.Hr(),
+                html.Div([
+                    html.H6("Restrict Nowcasting / News Aggregation Range : "),
+                    dcc.DatePickerRange(
+                        id='nowcasts-date-picker-range',
+                        month_format = 'D MMM YYYY',
+                        display_format='DD/MM/YYYY',
+                        # min_date_allowed = all_nowcast_dates[0],
+                        start_date = all_nowcast_dates[0],
+                        # max_date_allowed = all_nowcast_dates[-1],
+                        end_date = all_nowcast_dates[-1],
+                        style={"margin-bottom": "10px", "margin-left": "30px"}
+                    )], className="select-var-block"),
+                # dcc.RangeSlider(
+                #     min=0,
+                #     max=len(all_nowcast_dates)-1,
+                #     step=None,
+                #     marks= dict(zip(range(len(all_nowcast_dates)), 
+                #                 pd.to_datetime(nowcast.date).dt.strftime("%b-%d %Y"))) #,
+                #     # value=[all_nowcast_dates[0], all_nowcast_dates[-1]]
+                # ),
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(figure = {}, id='all-nowcasts-ts')
+                    ], width=6),
+                    dbc.Col([
+                        dcc.Graph(figure = {}, id='all-nowcasts-news')
+                    ], width=6),
+                ])
+        ]),
+        dcc.Tab(label='Nowcast Latest Quarter', children=[
             # dash_table.DataTable(data=nowcast.to_dict('records'), page_size=10),
             html.Br(),
-            html.H5("Nowcast for Current Quarter"),
+            html.H5("Nowcast for Latest Quarter"),
             html.Hr(),
             dbc.Row([
                 dcc.Graph(figure = {}, id='nowcast-qx')
@@ -118,53 +152,20 @@ app.layout = dbc.Container([
             ]) #,
             # html.Br()
         ]),
-        dcc.Tab(label='All Nowcasts', children=[
-                html.Br(),
-                html.H5("All Nowcasts and News Releases"),
-                html.Hr(),
-                html.Div([
-                    html.H6("Restrict Nowcasting / News Aggregation Range : "),
-                    dcc.DatePickerRange(
-                        id='nowcasts-date-picker-range',
-                        month_format = 'D MMM YYYY',
-                        display_format='DD/MM/YYYY',
-                        # min_date_allowed = all_nowcast_dates[0],
-                        start_date = all_nowcast_dates[0],
-                        # max_date_allowed = all_nowcast_dates[-1],
-                        end_date = all_nowcast_dates[-1],
-                        style={"margin-bottom": "10px", "margin-left": "30px"}
-                    )], className="select-var-block"),
-                # dcc.RangeSlider(
-                #     min=0,
-                #     max=len(all_nowcast_dates)-1,
-                #     step=None,
-                #     marks= dict(zip(range(len(all_nowcast_dates)), 
-                #                 pd.to_datetime(nowcast.date).dt.strftime("%b-%d %Y"))) #,
-                #     # value=[all_nowcast_dates[0], all_nowcast_dates[-1]]
-                # ),
-                dbc.Row([
-                    dbc.Col([
-                        dcc.Graph(figure = {}, id='all-nowcasts-ts')
-                    ], width=6),
-                    dbc.Col([
-                        dcc.Graph(figure = {}, id='all-nowcasts-news')
-                    ], width=6),
-                ])
-        ]),
         dcc.Tab(label='About the Nowcast', children=[
             # html.Hr(),
             html.Br(),
             html.H5("About the SA Nowcast"),
             html.P(["The South Africa Nowcast is a project that aims to provide a timely and accurate estimate of the current state of the South African economy. It is updated on a weekly basis and released every Friday."]),
             html.P(["The nowcast draws its data from the South Africa Macroeconomic Database (SAMADB) a free analytical macroeconomic database. SAMADB offers over 10 000 time series for South Africa collected from the Quarterly Bulletin, ",
-                    html.A("EconData", href='https://www.econdata.co.za'), ", and STATSSA, with weekly updates (every Thursday). It allows efficient global queries through API packages for ",
+                    html.A("EconData", href='https://www.econdata.co.za'), ", and STATSSA - with weekly updates (every Thursday). It allows efficient global queries through API packages for ",
                     html.A("R", href = "https://CRAN.R-project.org/package=samadb"), ", ",
                     html.A("Python", href = "https://pypi.org/project/samadb/"), ", and ",
                     html.A("Julia", href = "https://juliahub.com/ui/Search?q=SAMaDB&type=packages"), ", and provides concise and harmonized information about time series attributes and data availability/coverage. By harmonizing time series data and making it broadly accessible in a database, we hope to facilitate macroeconomic research in SA."]),
-            html.P(["The nowcast is based on a mixed-frequency dynamic factor model following Banbura & Modugno (2014) and Bok et al. (2018) (See ", 
+            html.P(["The nowcast is based on a mixed-frequency dynamic factor model following Banbura & Modugno (2014) and Bok et al. (2018) (see ", 
                     html.A("New York Fed Nowcasting Model", href='https://www.newyorkfed.org/research/policy/nowcast'), ") which is estimated using 54 monthly and 3 quarterly series. The model uses a Kalman filter, which allows for the inclusion of new data as it becomes available. Model-based news is computed as the difference between a new data release and its model-based forecast from the previous period. The impact of this news on the nowcast is given by a model-based weight following Banbura & Modugno (2014). In summary: nowcast revision = weight x news."]),        
              html.P(["The source code and data is publically available on ",
-                     html.A("GitHub", href = "https://github.com/Stellenbosch-Econometrics/SA-Nowcast"), " which includes weekly vintages of the nowcasting dataset available as excel files and all nowcasts and news releases as CSV files."]), 
+                     html.A("GitHub", href = "https://github.com/Stellenbosch-Econometrics/SA-Nowcast"), " which includes weekly vintages of the nowcasting dataset available as Excel files and all nowcasts and news releases as CSV files."]), 
             html.P(["More information about the database and nowcasting methodology is provided in the accompanying ", 
                     html.A("presentation slides", href = "https://raw.githubusercontent.com/Stellenbosch-Econometrics/SA-Nowcast/main/presentation/SAMADB_Nowcasting.pdf"), "."]),
             html.Br(),
@@ -185,9 +186,78 @@ app.layout = dbc.Container([
                     html.I("Annual Review of Economics, 10"), ", 615-643."])
         ]),
     ]),
-    html.Footer(["© 2023 Sebastian Krantz and Codera Analytics."], 
+    html.Footer(["© 2023 Sebastian Krantz and Codera Analytics"], 
                 style={'text-align': 'center', 'margin-top': '30px', 'margin-bottom': '20px', 'color': '#737373'})
 ], style={'max-width': '90%', 'margin': 'auto'})
+
+@callback(
+    Output('all-nowcasts-ts', 'figure'),
+    Output('all-nowcasts-news', 'figure'),
+    Input('nc-variable', 'value'),
+    Input('nowcasts-date-picker-range', 'start_date'),
+    Input('nowcasts-date-picker-range', 'end_date')
+)
+def update_allnc_graphs(var, start_date, end_date):
+    ## Adjusting the date range
+    nowcast_final_range = nowcast_final.loc[(nowcast_final.date >= start_date) & (nowcast_final.date <= end_date)]
+    nowcast_other_range = nowcast_other.loc[(nowcast_other.date >= start_date) & (nowcast_other.date <= end_date)]
+    gdp_dates = gdp_ld.index.to_timestamp()
+    gdp_ld_range = gdp_ld.loc[(gdp_dates >= start_date) & (gdp_dates <= end_date)]
+
+    ## Time series plot of all nowcasts 
+    nowcast_other_range["nc_date"] = nowcast_other_range.date.astype(str) + " : " + nowcast_other_range[var].round(2).astype(str) 
+    nowcast_other_range["nc_date_all"] = nowcast_other_range.groupby("quarter").nc_date.transform(lambda x: "<br>   " + "   <br>   ".join(reversed(x.tolist())))
+
+    fig = go.Figure()   
+
+    fig.add_trace(go.Scatter(x=nowcast_final_range.quarter,
+                             y=(np.exp(nowcast_final_range[var].rolling(4).sum()/100)-1)*100, 
+                             customdata=nowcast_final_range.date, 
+                             hovertemplate="<br>   %{customdata} : %{y:.2f}",
+                             mode='lines+markers', marker=dict(color='cyan'), name='Nowcast (YoY%)'))
+
+    fig.add_trace(go.Scatter(x=nowcast_other_range.quarter, y=(np.exp(nowcast_other_range[var]/100)**4-1)*100, 
+                            customdata=nowcast_other_range.nc_date_all, 
+                            # stackgroup = nowcast_other_range.quarter,
+                            hovertemplate= "%{customdata}", # "%{y:.2f} (%{customdata})",
+                            mode='markers', marker=dict(color='red'), name='Older Nowcasts'))
+    
+    fig.add_trace(go.Scatter(x=nowcast_final_range.quarter, y=(np.exp(nowcast_final_range[var]/100)**4-1)*100, 
+                    customdata=nowcast_final_range.date, 
+                    hovertemplate="<br>   %{customdata} : %{y:.2f}",
+                    mode='lines+markers', marker=dict(color='orange'), name='Nowcast'))
+
+    fig.add_trace(go.Scatter(x=gdp_ld_range.quarter, y=(np.exp(gdp_ld_range[var]/100)**4-1)*100, 
+                             hovertemplate="%{y:.2f}", 
+                             mode='lines+markers', marker=dict(color='green'), name='Outcome'))
+    
+    # Edit the layout
+    fig.update_layout(title='All Nowcasts (+ Backtesting 2019Q2-2022Q4)', 
+                      xaxis_title='Quarter', 
+                      yaxis_title='Quarterly Annualised Growth Rate (%)', 
+                      hovermode="x unified", 
+                      # hoverlabel = dict(namelength = 0),
+                      legend_traceorder="reversed",
+                      autosize=False, width=750, height=500, margin=dict(l=20, r=20, t=40, b=20), 
+                      template="plotly_dark")
+    
+    fig.update_xaxes(categoryorder='array', 
+                     categoryarray=nowcast_final_range.quarter)
+    
+    # News digest
+    news_tot = news.loc[(news.date >= start_date) & (news.date <= end_date) & (news["impacted variable"] == var)] \
+                .groupby(["sector_topic", "broad_sector", "topic"]) \
+                .agg({"weight": "mean", "impact": "mean"}).reset_index() 
+    news_tot["abs_impact"] = news_tot.impact.abs() # * 100
+
+    ## Barcharts of average news impacts
+    tot_news_fig = go.Figure()
+    tot_news_fig.add_bar(x=news_tot.sector_topic, y=news_tot.abs_impact * 4)
+    tot_news_fig.update_layout(title='Average Absolute News Impact',      
+                    xaxis_title='', yaxis_title='Average Absolute Impact',
+                    barmode='stack', hovermode="x", autosize=False, width=750, height=500,
+                    margin=dict(l=20, r=20, t=40, b=20), template="plotly_dark")
+    return fig, tot_news_fig
 
 @callback(
     Output('nowcast-qx', 'figure'),
@@ -229,72 +299,6 @@ def update_nccq_graphs(var, date):
     news_latest_quarter_dict[news_vars[3:8]] = news_latest_quarter_dict[news_vars[3:8]].transform(lambda x: x.round(3))
     news_latest_quarter_dict = news_latest_quarter_dict.rename(columns=news_labels)[list(news_labels.values())].to_dict('records')
     return fig_qx, news_latest_quarter_dict
-
-
-@callback(
-    Output('all-nowcasts-ts', 'figure'),
-    Output('all-nowcasts-news', 'figure'),
-    Input('nc-variable', 'value'),
-    Input('nowcasts-date-picker-range', 'start_date'),
-    Input('nowcasts-date-picker-range', 'end_date')
-)
-def update_allnc_graphs(var, start_date, end_date):
-
-    ## Adjusting the date range
-    nowcast_final_range = nowcast_final.loc[(nowcast_final.date >= start_date) & (nowcast_final.date <= end_date)]
-    nowcast_other_range = nowcast_other.loc[(nowcast_other.date >= start_date) & (nowcast_other.date <= end_date)]
-    gdp_dates = gdp_ld.index.to_timestamp()
-    gdp_ld_range = gdp_ld.loc[(gdp_dates >= start_date) & (gdp_dates <= end_date)]
-
-    ## Time series plot of all nowcasts 
-    nowcast_other_range["nc_date"] = nowcast_other_range.date.astype(str) + " : " + nowcast_other_range[var].round(2).astype(str) 
-    nowcast_other_range["nc_date_all"] = nowcast_other_range.groupby("quarter").nc_date.transform(lambda x: "<br>   " + "   <br>   ".join(reversed(x.tolist())))
-
-    fig = go.Figure()   
-
-    fig.add_trace(go.Scatter(x=nowcast_other_range.quarter, y=nowcast_other_range[var], 
-                            customdata=nowcast_other_range.nc_date_all, 
-                            # stackgroup = nowcast_other_range.quarter,
-                            hovertemplate= "%{customdata}", # "%{y:.2f} (%{customdata})",
-                            mode='markers', marker=dict(color='red'), name='Older Nowcasts'))
-    
-    fig.add_trace(go.Scatter(x=nowcast_final_range.quarter, y=nowcast_final_range[var], 
-                    customdata=nowcast_final_range.date, 
-                    hovertemplate="<br>   %{customdata} : %{y:.2f}",
-                    mode='lines+markers', marker=dict(color='orange'), name='Latest Nowcast'))
-    
-    fig.add_trace(go.Scatter(x=gdp_ld_range.quarter, y=gdp_ld_range[var], 
-                             hovertemplate="%{y:.2f}", 
-                             mode='lines+markers', name='Outcome'))
-    
-    # Edit the layout
-    fig.update_layout(title='All Nowcasts (+ Backtesting 2019Q2-2023Q1)', 
-                      xaxis_title='Quarter', 
-                      yaxis_title='Quarterly Log-Differnece Growth Rate (%)', 
-                      hovermode="x unified", 
-                      # hoverlabel = dict(namelength = 0),
-                      legend_traceorder="reversed",
-                      autosize=False, width=750, height=500, margin=dict(l=20, r=20, t=40, b=20), 
-                      template="plotly_dark")
-    
-    fig.update_xaxes(categoryorder='array', 
-                     categoryarray=nowcast_final_range.quarter)
-    
-    # News digest
-    news_tot = news.loc[(news.date >= start_date) & (news.date <= end_date) & (news["impacted variable"] == var)] \
-                .groupby(["sector_topic", "broad_sector", "topic"]) \
-                .agg({"weight": "mean", "impact": "mean"}).reset_index() 
-    news_tot["abs_impact"] = news_tot.impact.abs() # * 100
-
-    ## Barcharts of average news impacts
-    tot_news_fig = go.Figure()
-    tot_news_fig.add_bar(x=news_tot.sector_topic, y=news_tot.abs_impact)
-    tot_news_fig.update_layout(title='Average Absolute News Impact',      
-                    xaxis_title='Date', yaxis_title='Average Absolute Impact',
-                    barmode='stack', hovermode="x", autosize=False, width=750, height=500,
-                    margin=dict(l=20, r=20, t=40, b=20), template="plotly_dark")
-    return fig, tot_news_fig
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
